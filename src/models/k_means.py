8604a377aef7a82076ee085b2ada666821e6ecb8
monicammx <152923186+monicammx@users.noreply.github.com>
2023-12-06 21:15:14 -0500

    Update k_means.py

--- src/models/k_means.py
+++ src/models/k_means.py
@@ -1,18 +1,25 @@
+from sklearn.cluster import KMeans
 from model import Model
-import numpy as np
 
+class KMeansModel(Model):
 
-class KMeans(Model):
+    def __init__(self, constants):
+        super().__init__(constants)
+        self.name = "KMeans"
+        self.model = None
 
-	def __init__(self, constants):
-		super().__init__(constants)
-		self.name = "KMeans"
+    def find_optimal_hyperparameters(self, X, hyperparameters, cv=5):
+        # K-Means doesn't have hyperparameters to tune
+        pass
 
-	def find_optimal_hyperparameters(self, X, Y, hyperparameters, cv=5):
-		pass
+    def train(self, X, hyperparameters):
+        # Train the K-Means model with the specified hyperparameters
+        self.model = KMeans(n_clusters=hyperparameters.get('n_clusters', 8))
+        self.model.fit(X)
 
-	def train(self, X, Y, hyperparameters):
-		pass
+    def predict(self, X):
+        # Predict the cluster labels for input data X
+        if not self.model:
+            raise ValueError("The K-Means model has not been trained yet. Please call the train method.")
 
-	def predict(self, X):
-		pass
+        return self.model.predict(X)
